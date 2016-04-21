9639d409262848d106f2217cc08dc5edd6bad7e3
tmp <123>
2016-04-21 11:40:33 +0300

    v1.1

--- src/main/java/TemplateUtil.java
+++ src/main/java/TemplateUtil.java
@@ -1,8 +1,12 @@
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.event.implement.EscapeHtmlReference;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
+import javax.servlet.ServletException;
+import java.io.Writer;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -14,10 +18,18 @@ public class TemplateUtil {
         properties.setProperty(RuntimeConstants.RESOURCE_LOADER, "class");
         properties.setProperty("class.resource.loader.class",
                 ClasspathResourceLoader.class.getName());
-        Velocity.mergeTemplate(
-                template, "UTF-8", new VelocityContext(data), w
-        );
-        
+        properties.setProperty(RuntimeConstants.EVENTHANDLER_REFERENCEINSERTION, EscapeHtmlReference.class.getName());
+        Velocity.init(properties);
+    }
+
+    public static void render(String template, Map<String, Object> data, Writer w) throws ServletException {
+        try {
+            Velocity.mergeTemplate(template, "UTF-8", new VelocityContext(data), w);
+
+        } catch (Exception ex){
+            throw new ServletException(ex);
+        }
+
     }
 }
 
--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -3,6 +3,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by tmp on 14.04.2016.
@@ -13,28 +16,17 @@ public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setCharacterEncoding("UTF-8");
-        resp.getWriter().write("<html>\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "</head>\n" +
-                "<form action='/' method='POST'>\n" +
-                "    Ваше имя: <input name=\"name\">\n" +
-                "    <input type=\"submit\" value=\"OK\">\n" +
-                "</form>\n" +
-                "</html>");
+        Map<String, Object> data = Collections.emptyMap();
+        TemplateUtil.render("page1.vsl", data, resp.getWriter());
     }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         resp.setCharacterEncoding("UTF-8");
-        resp.getWriter().write("<html>\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<H1>Привет, " + name +"</H1>\n" +
-                "</body>\n" +
-                "</html>");
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("username", name);
+        TemplateUtil.render("page2.vsl", data, resp.getWriter());
 
     }
 }
