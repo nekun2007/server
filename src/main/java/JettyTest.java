9b1e2e649a51c8aa96fe87b6227311c5971d694c
tmp <SDFAS@ASDFG>
2016-05-12 11:49:56 +0300

    КОМЕНТЫ

--- src/main/java/JettyTest.java
+++ src/main/java/JettyTest.java
@@ -5,10 +5,11 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 public class JettyTest {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        ServletContextHandler ctx = new ServletContextHandler();
-        server.setHandler(ctx);
-        ctx.addServlet(TodoServlet.class, "/");
+        Server server = new Server(8088); //создаем объект сервер
+        ServletContextHandler handler = new ServletContextHandler(); //обработчик запросов
+        server.setHandler(handler);
+        handler.addServlet(TodoServlet.class, "/"); // слеш означает что обращение идет к корню сайта и перенаправляет на тудуСервлет
         server.start();
     }
 }
+// создание бд джетитест едит конфигуратионз плюсик апликатиеон org.h2.tools.Console name любое
--- src/main/java/TodoServlet.java
+++ src/main/java/TodoServlet.java
@@ -16,9 +16,12 @@ public class TodoServlet extends HttpServlet {
 
     protected void outputList(HttpServletResponse resp) throws ServletException, IOException {
         ArrayList<TodoItem> tasks = new ArrayList<>();
-        try (Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "")) {
-            try (PreparedStatement st = conn.prepareStatement("SELECT ID, TEXT FROM TODO ORDER BY ID DESC")) {
-                try (ResultSet rs = st.executeQuery()) {
+        try (Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", ""))  //по указанному адресу обращается к БД
+        {
+            try (PreparedStatement st = conn.prepareStatement("SELECT ID, TEXT FROM TODO ORDER BY ID DESC")) //запрос селект считывает даные из БД
+            {
+                try (ResultSet rs = st.executeQuery())// выполнение запроса и возвращает таблицу
+                {
                     while (rs.next()) {
                         int id = rs.getInt(1);
                         String text = rs.getString(2);
@@ -39,7 +42,7 @@ public class TodoServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         outputList(resp);
-    }
+    }//отображение по шаблону бд
 
 
     @Override
@@ -48,7 +51,7 @@ public class TodoServlet extends HttpServlet {
         String id = req.getParameter("id");
 //        tasks.add(newTask);
         try (Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "")) {
-            if (id !=null) {
+            if (id !=null) { //удаление записи если айди не пустое(айди идет снизу вверх)
                 try (PreparedStatement st = conn.prepareStatement("DELETE FROM TODO WHERE ID = ?")) {
                     st.setInt(1, Integer.parseInt(id));
                     st.execute();
@@ -66,5 +69,5 @@ public class TodoServlet extends HttpServlet {
         } catch (SQLException e) {
             throw new ServletException(e);
         }
-    }
+    }// нажатие на кнопку
 }
